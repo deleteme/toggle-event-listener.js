52b74ec47a2a1d333aca59739e6095b7a48f0cba
Matt Garrett <mattga@gmail.com>
2018-03-22 15:07:43 -0700

    add more tests

--- __tests__/toggle-event-listener-test.js
+++ __tests__/toggle-event-listener-test.js
@@ -8,33 +8,61 @@ describe('toggleEventListener()', () => {
     expect(toggleEventListener(element, 'click', handler)).toBe(element);
   });
 
-  it('should add the event handler if one is not already added.', () => {
-    expect.assertions(1);
-    const element = document.createElement('div');
-    const handler = jest.fn();
-    toggleEventListener(element, 'click', handler);
-    element.click();
-    expect(handler).toHaveBeenCalledTimes(1);
+  describe('adding', () => {
+    it('should add the event handler if one is not already added.', () => {
+      expect.assertions(1);
+      const element = document.createElement('div');
+      const handler = jest.fn();
+      toggleEventListener(element, 'click', handler);
+      element.click();
+      expect(handler).toHaveBeenCalledTimes(1);
+    });
+    it('should be able to add handlers of different types.', () => {
+      expect.assertions(2);
+      const element = document.createElement('div');
+      const handleClick = jest.fn();
+      const handleMouseMove = jest.fn();
+      toggleEventListener(element, 'click', handleClick);
+      toggleEventListener(element, 'mousemove', handleMouseMove);
+      element.click();
+      expect(handleClick).toHaveBeenCalledTimes(1);
+      expect(handleMouseMove).toHaveBeenCalledTimes(0);
+    });
+    it('should be able to add multiple handlers for the same event type', () => {
+      expect.assertions(2);
+      const element = document.createElement('div');
+      const handler1 = jest.fn();
+      const handler2 = jest.fn();
+      toggleEventListener(element, 'click', handler1);
+      toggleEventListener(element, 'click', handler2);
+      element.click();
+      expect(handler1).toHaveBeenCalledTimes(1);
+      expect(handler2).toHaveBeenCalledTimes(1);
+    });
   });
-  it('should be able to add handlers of different types.', () => {
-    expect.assertions(2);
-    const element = document.createElement('div');
-    const handleClick = jest.fn();
-    const handleMouseMove = jest.fn();
-    toggleEventListener(element, 'click', handleClick);
-    toggleEventListener(element, 'mousemove', handleMouseMove);
-    element.click();
-    expect(handleClick).toHaveBeenCalledTimes(1);
-    expect(handleMouseMove).toHaveBeenCalledTimes(0);
-  });
-  it('should remove the event handler if one is already added.', () => {
-    expect.assertions(1);
-    const element = document.createElement('div');
-    const handler = jest.fn();
-    toggleEventListener(element, 'click', handler);
-    toggleEventListener(element, 'click', handler);
-    element.click();
-    expect(handler).toHaveBeenCalledTimes(0);
+
+  describe('removing', () => {
+    it('should remove the event handler if one is already added.', () => {
+      expect.assertions(1);
+      const element = document.createElement('div');
+      const handler = jest.fn();
+      toggleEventListener(element, 'click', handler);
+      toggleEventListener(element, 'click', handler);
+      element.click();
+      expect(handler).toHaveBeenCalledTimes(0);
+    });
+    it('should be able to remove a single handler when multiple are registered for the same event type', () => {
+      expect.assertions(2);
+      const element = document.createElement('div');
+      const handler1 = jest.fn();
+      const handler2 = jest.fn();
+      toggleEventListener(element, 'click', handler1);
+      toggleEventListener(element, 'click', handler2);
+      toggleEventListener(element, 'click', handler2);
+      element.click();
+      expect(handler1).toHaveBeenCalledTimes(1);
+      expect(handler2).toHaveBeenCalledTimes(0);
+    });
   });
 
   describe('with declarative truthy state', () => {
